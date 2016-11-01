272c33591cba4961be92385ac260977e765c71da
Sergey Potapov <blake131313@gmail.com>
2016-11-01 18:19:20 +0100

    Finalaize trigrams

--- src/trigrams.rs
+++ src/trigrams.rs
@@ -1,15 +1,33 @@
 use std::collections::HashMap;
 
-pub fn count_trigrams(s : String) -> HashMap<String, u32> {
-    let mut s = s.to_lowercase();
+pub fn get_trigrams_with_positions(text : String) -> HashMap<String, u32> {
+    let counter_hash = count(text);
+
+    let mut count_vec: Vec<_> = counter_hash.iter().collect();
+    count_vec.sort_by_key(|key| key.1 );
+    count_vec.reverse();
+
+    let mut result: HashMap<String, u32> = HashMap::new();
+
+    // TODO: extract 600 as LANG_PROFILE_LENGTH * 2
+    for (i, trigram) in count_vec.iter().take(600).map(|x| x.0).enumerate() {
+        // TODO: find a way not to clone it
+        result.insert((*trigram).clone(), i as u32);
+    }
+
+    result
+}
+
+fn count(text : String) -> HashMap<String, u32> {
+    let mut s = text.to_lowercase();
     s.push(' '); // add space to the end
 
     let mut counter_hash : HashMap<String, u32> = HashMap::new();
 
     // iterate through the string and count trigrams
     let mut chars_iter = s.chars();
-    let mut c1 = chars_iter.next().unwrap();
-    let mut c2 = chars_iter.next().unwrap();
+    let mut c1 = ' ';
+    let mut c2 = to_trigram_char(chars_iter.next().unwrap());
     for cur_char in chars_iter {
         let c3 = to_trigram_char(cur_char);
         if !((c1 == ' ' && c2 == ' ') || (c2 == ' ' && c3 == ' ')) {
@@ -24,21 +42,10 @@ pub fn count_trigrams(s : String) -> HashMap<String, u32> {
         c2 = c3;
     }
 
-    let mut count_vec: Vec<_> = counter_hash.iter().collect();
-    count_vec.sort_by_key(|key| key.1 );
-
-    let mut result: HashMap<String, u32> = HashMap::new();
-
-    // TODO: extract 600 as LANG_PROFILE_LENGTH * 2
-    for (i, trigram) in count_vec.iter().take(600).map(|x| x.0).enumerate() {
-        // TODO: find a way not to clone it
-        result.insert((*trigram).clone(), i as u32);
-    }
-
-    result
+    counter_hash
 }
 
-// Replace punctuations and digits  with space.
+// Convert punctuations and digits to a space.
 fn to_trigram_char(ch : char) -> char {
     match ch {
         '\u{0000}'...'\u{0040}' | '\u{005B}'...'\u{0060}' | '\u{007B}'...'\u{007E}' => ' ',
@@ -51,41 +58,70 @@ fn to_trigram_char(ch : char) -> char {
 #[cfg(test)]
 mod tests {
     use super::to_trigram_char;
-    //use super::count_trigrams;
+    use super::count;
+    use super::get_trigrams_with_positions;
 
-    //fn assert_trigram_chars(chars : [char]) {
-    //}
+    fn assert_valuable_trigram_chars(chars : &[char]) {
+        for &ch in chars.iter() {
+            assert_eq!(to_trigram_char(ch), ch);
+        }
+    }
+
+    fn assert_not_valuable_trigram_chars(chars : &[char]) {
+        for &ch in chars.iter() {
+            assert_eq!(to_trigram_char(ch), ' ');
+        }
+    }
 
     #[test]
     fn test_to_trigram_char() {
-        assert_eq!(to_trigram_char('a'), 'a');
-        assert_eq!(to_trigram_char('z'), 'z');
-        assert_eq!(to_trigram_char('A'), 'A');
-        assert_eq!(to_trigram_char('Z'), 'Z');
-        assert_eq!(to_trigram_char('Ж'), 'Ж');
-        assert_eq!(to_trigram_char('ß'), 'ß');
+        // valuable chars, that metters
+        assert_valuable_trigram_chars(&['a', 'z', 'A', 'Z', 'Ж', 'ß']);
 
+        // punctuations, digits, etc..
+        //
         // 0x00 - 0x40
-        assert_eq!(to_trigram_char('\t'), ' ');
-        assert_eq!(to_trigram_char('\n'), ' ');
-        assert_eq!(to_trigram_char(' '), ' ');
-        assert_eq!(to_trigram_char('.'), ' ');
-        assert_eq!(to_trigram_char('0'), ' ');
-        assert_eq!(to_trigram_char('9'), ' ');
-        assert_eq!(to_trigram_char(','), ' ');
-        assert_eq!(to_trigram_char('@'), ' ');
-
+        assert_not_valuable_trigram_chars(&['\t', '\n', ' ', '.', '0', '9', ',', '@']);
         // 0x5B - 0x60
-        assert_eq!(to_trigram_char('['), ' ');
-        assert_eq!(to_trigram_char(']'), ' ');
-        assert_eq!(to_trigram_char('^'), ' ');
-        assert_eq!(to_trigram_char('\\'), ' ');
-        assert_eq!(to_trigram_char('`'), ' ');
-
+        assert_not_valuable_trigram_chars(&['[', ']', '^', '\\', '`']);
         // 0x7B - 0x7E
-        assert_eq!(to_trigram_char('|'), ' ');
-        assert_eq!(to_trigram_char('{'), ' ');
-        assert_eq!(to_trigram_char('}'), ' ');
-        assert_eq!(to_trigram_char('~'), ' ');
+        assert_not_valuable_trigram_chars(&['[', '|', '{', '}', '~']);
+    }
+
+
+
+    fn assert_count(text : &str, pairs : &[(&str, u32)]) {
+        let mut result = count(text.to_string());
+        for &(k, v) in pairs.iter() {
+            let &actual_val = result.get(k).unwrap_or(&0);
+            assert_eq!(actual_val, v, "trigram '{}' expected to occur {} times, got {}", k, v, actual_val);
+        }
+        assert_eq!(result.len(), pairs.len());
+    }
+
+    #[test]
+    fn test_count() {
+        assert_count("", &[]);
+        assert_count(",", &[]);
+        assert_count("a", &[(" a ", 1)]);
+        assert_count("-a-", &[(" a ", 1)]);
+        assert_count("yes", &[(" ye", 1), ("yes", 1), ("es ", 1)]);
+        assert_count("Give - IT...", &[(" gi", 1), ("giv", 1), ("ive", 1), ("ve ", 1), (" it", 1), ("it ", 1)]);
+    }
+
+    fn assert_get_trigrams_with_positions(text : &str, pairs : &[(&str, u32)]) {
+        let mut result = get_trigrams_with_positions(text.to_string());
+        for &(k, v) in pairs.iter() {
+            let &actual_val = result.get(k).unwrap_or(&0);
+            assert_eq!(actual_val, v, "trigram '{}' expected to occur {} times, got {}", k, v, actual_val);
+        }
+        assert_eq!(result.len(), pairs.len());
+    }
+
+    #[test]
+    fn test_get_trigrams_with_positions() {
+        let res = get_trigrams_with_positions("xaaaaabbbbd".to_string());
+        assert_eq!(*res.get("aaa").unwrap(), 0);
+        assert_eq!(*res.get("bbb").unwrap(), 1);
     }
 }
