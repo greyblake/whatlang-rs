96413555a71ab45f311990eae5464fa7f73d685b
Sergey Potapov <blake131313@gmail.com>
2017-07-29 18:11:30 +0200

    Add unit test for is_stop_char()

--- src/utils.rs
+++ src/utils.rs
@@ -6,3 +6,27 @@ pub fn is_stop_char(ch : char) -> bool {
         _ => false
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_is_top_char() {
+        // stop chars
+        assert!(is_stop_char(' '));
+        assert!(is_stop_char(','));
+        assert!(is_stop_char('-'));
+        assert!(is_stop_char('-'));
+        assert!(is_stop_char('9'));
+        assert!(is_stop_char('0'));
+
+        // non-stop chars
+        assert!(!is_stop_char('a'));
+        assert!(!is_stop_char('z'));
+        assert!(!is_stop_char('A')); // latin A
+        assert!(!is_stop_char('Z'));
+        assert!(!is_stop_char('я'));
+        assert!(!is_stop_char('А')); // cyrillic A
+    }
+}
