00f9351ce48739a4ddbc1d793779721e76319b62
Sergey Potapov <blake131313@gmail.com>
2017-07-31 23:03:19 +0200

    Update CLI example

--- examples/cli.rs
+++ examples/cli.rs
@@ -1,15 +1,17 @@
 extern crate whatlang;
 
 use std::io;
-use whatlang::{detect_lang};
+use whatlang::detect;
 
 fn main() {
     let mut text = String::new();
-    println!("Please enter a text: ");
+    println!("Please enter a text:");
     io::stdin().read_line(&mut text).expect("Failed to read line");
 
-    if let Some(lang) = detect_lang(&text) {
-        println!("Language: {}", lang);
+    if let Some(info) = detect(&text) {
+        println!("Language: {}", info.lang());
+        println!("Confidence: {:2.0}%", info.confidence() * 100.0);
+        println!("Is reliable: {}", info.is_reliable());
     } else {
         println!("Cannot recognize a language :(");
     }
