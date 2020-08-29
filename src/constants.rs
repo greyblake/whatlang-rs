b804dd42ff2b9ce51c9e69fb85c4e0c38f6d28c2
Sergey Potapov <sergey.potapov@xbav.de>
2020-08-29 14:27:55 +0200

    Calculate MAX_TOTAL_DISTANCE const dynamically

--- src/constants.rs
+++ src/constants.rs
@@ -2,7 +2,7 @@
 pub const MAX_TRIGRAM_DISTANCE: u32 = 300;
 
 // 300 trigrams where each has MAX_TOTAL_DISTANCE=300, gives us 90_000.
-pub const MAX_TOTAL_DISTANCE: u32 = 90_000;
+pub const MAX_TOTAL_DISTANCE: u32 = MAX_TRIGRAM_DISTANCE * MAX_TRIGRAM_DISTANCE;
 
 // Double MAX_TRIGRAM_DISTANCE
 pub const TEXT_TRIGRAMS_SIZE: usize = 600;
